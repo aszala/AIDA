aaa85420c446f4aa6148e1afcd6b30d051d973ba
Abhay Zala <zala.abhay@gmail.com>
2020-04-15 17:59:18 -0400

    Stuff

--- gen_pcd.py
+++ gen_pcd.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     print("Read Redwood dataset")
-    color_raw = o3d.io.read_image("456443.jpg")
-    depth_raw = o3d.io.read_image("456443_1_depth.png")
+    color_raw = o3d.io.read_image("./color/00003.jpg")
+    depth_raw = o3d.io.read_image("./depth/00003.png")
     rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
         color_raw, depth_raw)
     print(rgbd_image)
